4b508d4dd3f722765496bc09dc046f0a48346963
surajkr727785 <surajkr727785@gmail.com>
2021-03-29 22:08:55 +0530

    completed

---
+++ src/views/AnotherPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="admin">
+    <header class="admin__header">
+      <a href="/viewAllJobs">
+        <img class="logo" src="../assets/logo.png" alt="" />
+      </a>
+    </header>
+    <nav class="admin__nav">
+      <ul class="menu">
+        <li class="menu__item">
+          <router-link class="menu_link" to="/">Dashboard</router-link> |
+        </li>
+        <li class="menu__item">
+          <router-link class="menu_link" to="/another"
+            >Another Page</router-link
+          >
+        </li>
+      </ul>
+    </nav>
+    <main class="admin__main">
+      <h2>Another page</h2>
+      <div class="dashboard">
+        <h3>This is Second page</h3>
+      </div>
+    </main>
+    <footer class="admin__footer">
+      <span> &copy; 2021 Company Inc </span>
+    </footer>
+  </div>
+</template>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="admin">
+    <header class="admin__header">
+      <a href="/viewAllJobs">
+        <img class="logo" src="../assets/logo.png" alt="" />
+      </a>
+    </header>
+    <nav class="admin__nav">
+      <ul class="menu">
+        <li class="menu__item">
+          <a class="menu_link" href="/">Dashboard</a> |
+        </li>
+        <li class="menu__item">
+          <a class="menu_link" href="/another">Another Page</a>
+        </li>
+      </ul>
+    </nav>
+    <main class="admin__main">
+      <h2>Dashboard</h2>
+      <div class="dashboard">
+        <h3>This is Dashboard</h3>
+      </div>
+    </main>
+    <footer class="admin__footer">
+      <span> &copy; 2021 Company Inc. </span>
+    </footer>
+  </div>
+</template>
